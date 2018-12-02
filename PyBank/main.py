7cb9f6983f7ce7d97a08f88682ab5c3bb4b598bd
usama-ali <43964490+usama-ali@users.noreply.github.com>
2018-12-01 23:08:40 -0500

    Add files via upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,33 +2,60 @@ import os
 import csv
 import numpy as np
 
-file = open("budget_data.csv", "r")
-data = {}
-sum = 0
-for line in file:
-    x = line.split(",")
-    a = x[0]
-    b = x[1]
-    data[a]= b
-    sum += data[a]
+# input and ourput files
+data = os.path.join(".", "budget_data.csv")
+output = os.path.join(".", "budget_output.txt")
 
-#print(f' {data[1]}')
+# initial values and defining variables as lists
+total_months = 0
+month_of_change_ls = []
+change_ls = []
+max_increase_ls = ["", 0]
+max_decrease_ls = ["", 99999999999999]
+total = 0
 
+with open(data) as csvdata:
+    csvreader = csv.reader(csvdata)
+    # reading the first row (header)
+    header = next(csvreader)
+    # reading the second row (1st row data)
+    first_row = next(csvreader)
+    # update the number of total months read and current & total profit/loss net
+    total_months = total_months + 1
+    prev_tot = int(first_row[1])
+    total = total + prev_tot
 
-def average(numbers):
-    length= len(numbers)
-    total = 0.0
+    # for loop for 2nd row of the data
+    for row in csvreader:
+        total_months = total_months + 1
+        total = total + int(row[1])
 
-    for number in numbers:
-        total = total + number # total += number
-        arithmatic_average = total/length
-    return arithmatic_average
+        change = int(row[1]) - prev_tot
+        prev_tot = int(row[1])
+
+        change_ls = change_ls + [change]
+        month_of_change_ls = month_of_change_ls + [row[0]]
+
+        # get the greatest increase
+        if change > max_increase_ls[1]:
+            max_increase_ls[0] = row[0]
+            max_increase_ls[1] = change
+
+        # get the greatest decrease
+        if change < max_decrease_ls[1]:
+            max_decrease_ls[0] = row[0]
+            max_decrease_ls[1] = change
+
+# return arithmatic average of change over the whole period
+mean_change = sum(change_ls)/len(change_ls)
 
 
 print(f"Financial Analysis")
 print(f"----------------------")
-print(f"Total Months: len(data)-1")
-print(f"Total: sum")
-print(f"Average  Change: sum(data[1])/len(data[1])")
-#print(f"Greatest Increase in Profits: {max_increase_date} ($ {max_increase_value})")
-#print(f"Greatest Increase in Profits: {max_decrease_date} ($ {max_decrease_value})")
+print(f"Total Months: {total_months}")
+print(f"Total: {total}")
+print(f"Average  Change: ${mean_change:.2f}")
+print(f"Greatest Increase in Profits: {max_increase_ls[0]} ($ {max_increase_ls[1]})")
+print(f"Greatest Decrease in Profits: {max_decrease_ls[0]} ($ {max_decrease_ls[1]})")
+
+
